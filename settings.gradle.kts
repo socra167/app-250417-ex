31de31059b030c681c27c61b7ba1e637393ecfd9
socra167 <socra167@gmail.com>
2025-04-17 12:26:23 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "app-250417-ex"
---
+++ src/main/kotlin/com/app250417ex/App250417ExApplication.kt
@@ -0,0 +1,11 @@
+package com.app250417ex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App250417ExApplication
+
+fun main(args: Array<String>) {
+    runApplication<App250417ExApplication>(*args)
+}
